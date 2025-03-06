6643d19d2fb5cc87c51ff4fdef997481930a61f3
CaioPessoa <caiopessoa1298@gmail.com>
2025-03-06 20:12:14 -0300

    feat: add ShoppingItemCard and set deployment target to iOS 15

---
+++ Sources/PomodoroDesignSystem/ShoppingItemCard.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PomodoroDesignSystem
+//
+//  Created by Caio de Almeida Pessoa on 06/03/25.
+//
+
+import SwiftUI
+
+struct ShoppingItemCard: View {
+    var body: some View {
+        Text("ShoppingItemCard")
+    }
+}
+
+#Preview {
+    ShoppingItemCard()
+}
